ef3dc6aacae093c9e5b5b13197c707bc40074a32
Megan Arellano <109243577+megan-arellano@users.noreply.github.com>
2024-10-22 11:38:14 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-ln -s $(pwd)/zshrc $HOME/.zshrc
-ln -s $(pwd)/bash_profile $HOME/.bash_profile
+ln -s $(pwd)/.zshrc $HOME/.zshrc
+ln -s $(pwd)/.bash_profile $HOME/.bash_profile
 
